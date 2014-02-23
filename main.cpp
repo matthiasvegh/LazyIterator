54e36e2f1effabb143f7fb83cb4ca949cd3f6b86
Matthias Vegh <matyas.vegh@gmail.com>
2014-02-23 16:30:56 +0100

    Added composed iterator tests on a vector.

--- main.cpp
+++ main.cpp
@@ -74,6 +74,48 @@ int main() {
 		std::cout<< "ph::find with two untils: " << (end-start).count() << std::endl;
 	}
 
+
+	std::vector<int> v(1024);
+	for(auto& i: v) {
+		std::uniform_int_distribution<int> intDis(0, 65536);
+		i = intDis(gen);
+	}
+
+	// TODO: get rid of this requirement.
+	v[1023] = 200;
+
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+
+		for(auto it = v.begin(); it != v.end(); ++it) {
+			if(*it == 200)
+				break;
+			if(*it == 100)
+				break;
+			if(*it == 300)
+				break;
+		}
+
+		auto end = std::chrono::high_resolution_clock::now();
+		std::cout << "vectorFind: " << (end - start).count() << std::endl;
+
+	}
+
+	{
+		auto start = std::chrono::high_resolution_clock::now();
+
+		auto endIterator1 = ph::Until([](const int& i) { return i==100; });
+		auto endIterator2 = ph::Until([](const int& i) { return i==200; });
+
+		auto endIterator = endIterator1 && endIterator2;
+
+		ph::find(v.begin(), endIterator, 300);
+
+		auto end = std::chrono::high_resolution_clock::now();
+		std::cout << "phFind: " << (end - start).count() << std::endl;
+
+	}
+
 }
 
 
