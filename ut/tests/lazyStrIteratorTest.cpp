e111e4821e6d6cc9ce177cd9941c38ad55aed209
Matthias Vegh <matyas.vegh@gmail.com>
2014-02-25 20:40:20 +0100

    Refactored unit test structure (lazyStrIterator has its own file now. mainTest->untilTest)

---
+++ ut/tests/lazyStrIteratorTest.cpp
@@ -0,0 +1,37 @@
+#include <boost/test/unit_test.hpp>
+#include "ph.hpp"
+#include "algorithm.hpp"
+
+#include <algorithm>
+#include <cstring>
+#include <functional>
+
+BOOST_AUTO_TEST_SUITE(LazyStrTests)
+
+BOOST_AUTO_TEST_CASE(Find_With_LazyStrIterator_should_behave_as_if_using_strlen) {
+
+	const char* str = "1234567890";
+
+	auto posByPh = ph::find(str, ph::LazyStrIterator{}, '6');
+
+	BOOST_REQUIRE(posByPh != str+strlen(str));
+
+	BOOST_CHECK_EQUAL(*posByPh, '6');
+
+}
+
+BOOST_AUTO_TEST_CASE(LazyStrIterator_should_compare_equal_to_an_iterator_pointing_to_NULL) {
+	const char* c = "\0";
+
+	BOOST_CHECK(ph::LazyStrIterator{} == c);
+
+}
+
+BOOST_AUTO_TEST_CASE(LazyStrIterator_should_not_compare_equal_to_an_iterator_not_pointing_to_NULL) {
+	const char* c = "a";
+
+	BOOST_CHECK(ph::LazyStrIterator{} != c);
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
--- ut/tests/untilTest.cpp
+++ ut/tests/untilTest.cpp
@@ -10,32 +10,6 @@
 BOOST_AUTO_TEST_SUITE(mainTests)
 
 
-BOOST_AUTO_TEST_CASE(Find_With_LazyStrIterator_should_behave_as_if_using_strlen) {
-
-	const char* str = "1234567890";
-
-	auto posByPh = ph::find(str, ph::LazyStrIterator{}, '6');
-
-	BOOST_REQUIRE(posByPh != str+strlen(str));
-
-	BOOST_CHECK_EQUAL(*posByPh, '6');
-
-}
-
-BOOST_AUTO_TEST_CASE(LazyStrIterator_should_compare_equal_to_an_iterator_pointing_to_NULL) {
-	const char* c = "\0";
-
-	BOOST_CHECK(ph::LazyStrIterator{} == c);
-
-}
-
-BOOST_AUTO_TEST_CASE(LazyStrIterator_should_not_compare_equal_to_an_iterator_not_pointing_to_NULL) {
-	const char* c = "a";
-
-	BOOST_CHECK(ph::LazyStrIterator{} != c);
-
-}
-
 BOOST_AUTO_TEST_CASE(until_with_value_predicate_should_compare_equal_to_iterator_pointing_to_that_value) {
 	std::vector<int> v;
 	v.push_back(1);
