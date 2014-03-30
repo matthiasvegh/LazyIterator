a45811507c713afaebb517118fc0a94c8c009a52
Matthias Vegh <matyas.vegh@gmail.com>
2014-03-30 19:55:55 +0200

    Added map_values range adaptor.

--- include/adaptor/map.hpp
+++ include/adaptor/map.hpp
@@ -18,7 +18,20 @@ namespace detail {
 
 	};
 
-	struct dummy_map_range { };
+	template<typename Iterator>
+	class map_value_iterator: public std::decay<Iterator>::type {
+	public:
+		using Base = typename std::decay<Iterator>::type;
+		map_value_iterator(Iterator it): Base(it) {}
+		map_value_iterator() = default;
+
+		const auto& operator*() const { return Base::operator*().second; }
+		auto& operator*() { return Base::operator*().second; }
+
+	};
+
+	struct dummy_map_keys_range { };
+	struct dummy_map_values_range { };
 } // namespace detail
 
 template<typename Begin, typename End>
@@ -41,12 +54,38 @@ public:
 
 };
 
-const static auto map_keys = detail::dummy_map_range{};
+template<typename Begin, typename End>
+class Map_Values {
+	detail::map_key_iterator<Begin> b;
+	detail::map_key_iterator<End> e;
+
+public:
+
+	Map_Values() = default;
+	Map_Values(Begin _b, End _e):
+		b(detail::map_value_iterator<Begin>{_b}),
+		e(detail::map_value_iterator<End>{_e}) {}
+
+	detail::map_key_iterator<Begin>& begin() { return b; }
+	const detail::map_key_iterator<Begin>& begin() const { return b; }
+
+	detail::map_key_iterator<End>& end() { return e; }
+	const detail::map_key_iterator<End>& end() const { return e; }
+
+};
+
+const static auto map_keys = detail::dummy_map_keys_range{};
+const static auto map_values = detail::dummy_map_values_range{};
 
 }} // namespace ph::adaptor
 
 template<typename Range>
-auto operator|(Range r, ph::adaptor::detail::dummy_map_range) {
+auto operator|(Range r, ph::adaptor::detail::dummy_map_keys_range) {
+	return ph::adaptor::Map_Keys<decltype(r.begin()), decltype(r.end())>{r.begin(), r.end()};
+}
+
+template<typename Range>
+auto operator|(Range r, ph::adaptor::detail::dummy_map_values_range) {
 	return ph::adaptor::Map_Keys<decltype(r.begin()), decltype(r.end())>{r.begin(), r.end()};
 }
 
--- ut/tests/adaptorTests.cpp
+++ ut/tests/adaptorTests.cpp
@@ -45,4 +45,44 @@ BOOST_AUTO_TEST_CASE(Map_Keys_Pipe_should_work_on_empty_range) {
 }
 
 
+BOOST_AUTO_TEST_CASE(Map_Values_Range_should_be_empty_if_input_range_was_empty) {
+	std::map<int, int> m;
+	ph::adaptor::Map_Values<decltype(m.begin()), decltype(m.end())> mk{m.begin(), m.end()};
+	bool called = false;
+	ph::for_each(mk.begin(), mk.end(), [&called](decltype(*mk.begin()) v) {
+			called = true;
+			(void)v;
+	});
+	BOOST_CHECK_EQUAL(called, false);
+}
+
+BOOST_AUTO_TEST_CASE(Map_Values_Range_should_not_be_empty_if_input_range_was_not_empty) {
+	std::map<int, int> m;
+	m.insert({1, 1});
+	m.insert({2, 2});
+	ph::adaptor::Map_Values<decltype(m.begin()), decltype(m.end())> mk{m.begin(), m.end()};
+	bool called = false;
+	ph::for_each(mk.begin(), mk.end(), [&called](decltype(*mk.begin()) v) {
+			called = true;
+			(void)v;
+	});
+	BOOST_CHECK_EQUAL(called, true);
+}
+
+BOOST_AUTO_TEST_CASE(Map_Values_Pipe_should_work_on_empty_range) {
+	std::map<int, int> m;
+
+	auto map_range = ph::make_iterator_range(m.begin(), m.end());
+	auto range = map_range | ph::adaptor::map_values;
+
+	bool called = false;
+	ph::for_each(range.begin(), range.end(), [&called](decltype(*range.begin()) v) {
+			called = true;
+			(void)v;
+	});
+	BOOST_CHECK_EQUAL(called, false);
+
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
